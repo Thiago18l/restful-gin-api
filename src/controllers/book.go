d68c3fe42c9ed1f1c29c23cc1a732371e29aa243
Thiago L <thiago.lopes.dev@gmail.com>
2021-08-27 15:26:05 -0300

    feat: add some CRUD requests

--- src/controllers/book.go
+++ src/controllers/book.go
@@ -33,3 +33,50 @@ func CreateBook(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"data": book})
 }
+
+//GET /books/:id
+// Find single book
+func FindBook(c *gin.Context) {
+	db := c.MustGet("db").(*gorm.DB)
+	var book models.Book
+	if err := db.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+//PATCH /books/:id
+//Update a book
+func UpdateBook(c *gin.Context) {
+	db := c.MustGet("db").(*gorm.DB)
+	var book models.Book
+	if err := db.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+	}
+
+	//validate input
+	var input models.UpdateBookInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	db.Model(&book).Updates(input)
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+//DELETE /book/:id
+// Delete a book
+func DeleteBook(c *gin.Context) {
+	db := c.MustGet("db").(*gorm.DB)
+
+	var book models.Book
+	if err := db.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "record not found!"})
+		return
+	}
+
+	db.Delete(&book)
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
